9c6c3f7554e984023010e7b70cc14be42ba815ef
Gabriel <gabrielgta7000@gmail.com>
2025-12-14 11:15:35 -0300

    Remoção do using namespace std no arquivo main

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,68 @@
 #include <iostream>
+#include <string>
 
 #include "functions.h"
-using namespace std;
+// using namespace std;
 
 int main()
 {
-    string continuar;
-    string personagem1, personagem2;
-    string arms;
+    std::string continuar;
+    std::string personagem1, personagem2;
+    std::string arms;
     int vida2 = 200;
     int vida1 = 200;
     int dano;
     int turno = 1;
 
-    cout << "Digite o nome do primeiro personagem: ";
-    getline(cin, personagem1);
-    cout << "Digite o nome do segundo personagem: ";
-    getline(cin, personagem2);
+    std::cout << "Digite o nome do primeiro personagem: ";
+    std::getline(std::cin, personagem1);
+    std::cout << "Digite o nome do segundo personagem: ";
+    std::getline(std::cin, personagem2);
 
     while (personagem1 == personagem2){
-        cout << "Personagem com o mesmo nome\nInsira um nome diferente\n";
-        getline(cin, personagem2);
+        std::cout << "Personagem com o mesmo nome\nInsira um nome diferente\n";
+        std::getline(std::cin, personagem2);
     }
 
     while(true)
     {
-        cout << "=== TURNO " << turno << "\n";
+        std::cout << "=== TURNO " << turno << "\n";
 
-        cout << "Vez de " << personagem1 << " jogar\n";
+        std::cout << "Vez de " << personagem1 << " jogar\n";
         mostrarArmas();
-        cout << "Digite a arma de combate: ";
-        cin >> arms;
+        std::cout << "Digite a arma de combate: ";
+        std::cin >> arms;
 
         dano = calculardano(arms);
 
         vida2 = calcularvida(vida2, dano);
 
-        cout << "Foi desferido " << dano << " de dano!\n";
+        std::cout << "Foi desferido " << dano << " de dano!\n";
 
-        cout << "Vida atual de " << personagem2 << ": " << vida2 << "\n";
+        std::cout << "Vida atual de " << personagem2 << ": " << vida2 << "\n";
         mostrarVida(personagem2, vida2);
 
         if (verificarFim(vida1, vida2, personagem1, personagem2)) break;
 
 
 
-        cout << "Vez de " << personagem2 << " jogar\n";
+        std::cout << "Vez de " << personagem2 << " jogar\n";
         mostrarArmas();
-        cout << "Digite a arma de combate: ";
-        cin >> arms;
+        std::cout << "Digite a arma de combate: ";
+        std::cin >> arms;
 
         int danoinimigo = calculardano(arms);
 
         vida1 = calcularvida(vida1, danoinimigo);
-        cout << "Foi desferido " << danoinimigo << " de dano\n";
+        std::cout << "Foi desferido " << danoinimigo << " de dano\n";
 
-        cout << "Vida atual de " << personagem1 << ": " << vida1 << "\n";
+        std::cout << "Vida atual de " << personagem1 << ": " << vida1 << "\n";
         mostrarVida(personagem1, vida1);
 
         if (verificarFim(vida1, vida2, personagem1, personagem2)) break;
 
         if (!querContinuar()) {
-            cout << "\nJogo encerrado...\n";
+            std::cout << "\nJogo encerrado...\n";
             break;
         }
 
